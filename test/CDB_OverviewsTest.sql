b2602ac0d0c3e1f79c9f23e1339d2e404794f4f4
Tyler Parsons <tparsons17@bloomberg.net>
2019-01-14 12:33:10 -0500

    Support optional disable flag for overviews, off by default

--- test/CDB_OverviewsTest.sql
+++ test/CDB_OverviewsTest.sql
@@ -28,6 +28,30 @@ SELECT CDB_Overviews(ARRAY['base_t'::regclass, 'base_bare_t'::regclass]);
 SELECT CDB_Overviews('polyg_t'::regclass);
 SELECT CDB_Overviews('column_types_t'::regclass);
 
+-- Disable overviews - no results should be expected
+INSERT INTO cdb_conf VALUES ('disable_overviews', 'true'::json);
+SELECT CDB_Overviews('base_t'::regclass);
+SELECT CDB_Overviews('"public"."base_t"'::regclass);
+SELECT CDB_Overviews(ARRAY['base_t'::regclass, 'base_bare_t'::regclass]);
+SELECT CDB_Overviews('polyg_t'::regclass);
+SELECT CDB_Overviews('column_types_t'::regclass);
+
+-- Reenable overviews with "false" value in table
+UPDATE cdb_conf SET value = 'false'::json WHERE key = 'disable_overviews';
+SELECT CDB_Overviews('base_t'::regclass);
+SELECT CDB_Overviews('"public"."base_t"'::regclass);
+SELECT CDB_Overviews(ARRAY['base_t'::regclass, 'base_bare_t'::regclass]);
+SELECT CDB_Overviews('polyg_t'::regclass);
+SELECT CDB_Overviews('column_types_t'::regclass);
+
+-- Remove disable flag completely - overviews should be reenabled
+DELETE FROM cdb_conf WHERE key = 'disable_overviews';
+SELECT CDB_Overviews('base_t'::regclass);
+SELECT CDB_Overviews('"public"."base_t"'::regclass);
+SELECT CDB_Overviews(ARRAY['base_t'::regclass, 'base_bare_t'::regclass]);
+SELECT CDB_Overviews('polyg_t'::regclass);
+SELECT CDB_Overviews('column_types_t'::regclass);
+
 SELECT CDB_DropOverviews('column_types_t'::regclass);
 SELECT CDB_DropOverviews('base_bare_t'::regclass);
 SELECT CDB_DropOverviews('base_t'::regclass);
